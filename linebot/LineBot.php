7c03c7fee75a6bab0540d98691a61da01b2477db
gugu <hide-book@hide-book.local>
2016-12-08 15:23:30 +0900

    ・LINE BOT API Trialで使用しなくなった箇所を削除（画像送付機能もついでに削除） ・変数名をキャメル型に修正 ・ログ表示の修正

--- linebot/LineBot.php
+++ linebot/LineBot.php
@@ -3,136 +3,57 @@
 class LineBot
 {
 	const API_URL = 'https://api.line.me/v2/bot/message/reply';
-	//const TO_CHANNEL = '';
-	//const EVENT_TYPE = '';
-
-	//public $channel_id;
-	//public $channel_secret;
-	//public $channel_mid;
 	public $access_token;
 	public $response_header;
-	public $response_text;
-	public $response_to;
+	public $reply_token;
+	public $user_text;
 
-	//public function __construct($channel_id = null, $channel_secret = null, $channel_mid = null)
 	public function __construct($access_token = null)
 	{
-		//$this->channel_id = $channel_id;
-		//$this->channel_secret = $channel_secret;
-		//$this->channel_mid = $channel_mid;
 		$this->access_token = $access_token;
-
+		// headerの作成
 		$this->response_header = array(
 			'Content-Type: application/json;',
-			//'X-Line-ChannelID: ' . $this->channel_id,
-			//'X-Line-ChannelSecret: ' . $this->channel_secret,
-			//'X-Line-Trusted-User-With-ACL: ' . $this->channel_mid,
 			'Authorization: Bearer ' . $this->access_token
 		);
 
-		$content = $this->getContent();
-		$this->response_to = $content->from;
-		$this->response_text = $content->text;
+		$event = $this->getEvent();
+		// replytokenの取得
+		$reply_token = $event->{"replyToken"};
+		// ユーザーから送られてきた本文を取得
+		$user_text = $event->{"message"}->{"text"};
 	}
 
-	public function getContent()
-	{
-		$contents = file_get_contents('php://input');
-		$json = json_decode($contents);
-		$content = $json->result{0}->content;
-
-		return $content;
+	// ユーザーから送られてきた情報を取得
+	private function getEvent(){
+		$obj = json_decode(file_get_contents("php://input"));
+		$event = $obj->{"events"}[0];
+		return $event;
 	}
 
+	// ユーザーにテキストを送る。
 	public function sendText($format = null)
 	{
-		$response = array(
-			'contentType' => 1,
-			'toType' => 1,
-			'text' => $text
-		);
-
-		$post_data = array(
-			'to' => array($this->response_to),
-			//'toChannel' => self::TO_CHANNEL,
-			//'eventType' => self::EVENT_TYPE,
-			'content' => $response
-		);
-		//callback確認
-		$obj = json_decode(file_get_contents('php://input'));
-
-		$event = $obj->{"events"}[0];
-		$response = $event->{"message"}->{"text"};
-		$replyToken = $event->{"replyToken"};
-
+		$response_text = "";
 		if ($format) {
-			$text = sprintf($format, $response);
+			$response_text = sprintf($format, $user_text);
 		} else {
-			$text = $response;
+			$response_text = $user_text;
 		}
 
 		$post = [
-			"replyToken" => $replyToken,
+			"replyToken" => $reply_token,
 			"messages" => array(
 				array(
 					"type" => "text",
-					"text" => $text
+					"text" => $response_text
 				)
 			)
 		];
 		return $this->sendMessage($post);
 	}
 
-	public function sendImage($account_key = null, $text = null)
-	{
-		if (!$account_key) {
-			$this->sendText('チョウシガワルイデス...');
-			return;
-		}
-
-		if (!$text) {
-			$text = $this->response_text;
-		}
-
-		$keyword = urlencode("'" . $text . "'");
-		$credencial = 'Authorization: Basic ' . base64_encode($account_key . ":" . $account_key);
-		$context = stream_context_create(array(
-			'http' => array(
-				'header' => $credencial
-			)
-		));
-		$contents = file_get_contents('https://api.datamarket.azure.com/Bing/Search/Image?$format=json&$top=10&Adult='."'".'Strict'."'".'&Query='.$keyword, 0, $context);
-		$json = json_decode($contents);
-
-		if (!$json->d->results) {
-			$this->sendText('ワカリマセンデシタ...');
-			return;
-		}
-
-		$images = array();
-		foreach ($json->d->results as $result) {
-			$images[] = array($result->MediaUrl, $result->Thumbnail->MediaUrl);
-		}
-		$rand = rand(0, count($images)-1);
-		$image_url = $images[$rand][0];
-		$image_thumb_url = $images[$rand][1];
-
-		$response = array(
-			'contentType' => 2,
-			'toType' => 1,
-			'originalContentUrl' => $image_url,
-			'previewImageUrl' => $image_thumb_url
-		);
-
-		$post_data = array(
-			'to' => array($this->response_to),
-			'toChannel' => self::TO_CHANNEL,
-			'eventType' => self::EVENT_TYPE,
-			'content' => $response
-		);
-
-		return $this->sendMessage($post_data);
-	}
+	// Message APIを使用してユーザーにLineを送る。
 	public function sendMessage($post_data)
 	{
 		$ch = curl_init(self::API_URL);
@@ -143,7 +64,9 @@ class LineBot
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $this->response_header);
 		$result = curl_exec($ch);
 		curl_close($ch);
-		error_log($ch, 3, '../logs/result.log');
+		// logの出力
+		error_log(print_r('[' . date("Y-m-d H:i:s") . ']' . $result . "\n", true), 3, '../logs/result.log');
+
 		return $result;
 	}
 }
